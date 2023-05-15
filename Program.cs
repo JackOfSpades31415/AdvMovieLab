435bc3b7ba622ff25d5f4754824c0c559bc9710c
JackOfMinisculeTrades <jack314159265358979323@gmail.com>
2023-05-15 14:42:44 -0500

    Get Nlogged

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+Console.WriteLine("Hello World!");
