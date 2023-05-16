da385e0d738627dc0a272810a31934474d0215d9
JackOfMinisculeTrades <jack314159265358979323@gmail.com>
2023-05-16 00:16:28 -0500

    putting in file scrubber

--- Program.cs
+++ Program.cs
@@ -7,9 +7,34 @@ Movie movie = new Movie
 {
     mediaId = 123,
     title = "Greatest Movie Ever, The (2023)",
+     director = "Jeff Grissom",
+    runningTime = new TimeSpan(2, 21, 23), //hours, minutes, seconds
     genres = { "Comedy", "Romance" }
 };
 
 Console.WriteLine(movie.Display());
+
+Album album = new Album
+{
+    mediaId = 321,
+    title = "Greatest Album Ever, The (2020)",
+    artist = "Jeff's Awesome Band",
+    recordLabel = "Universal Music Group",
+    genres = { "Rock" }
+};
+Console.WriteLine(album.Display());
+
+Book book = new Book
+{
+    mediaId = 111,
+    title = "Super Cool Book",
+    author = "Jeff Grissom",
+    pageCount = 101,
+    publisher = "",
+    genres = { "Suspense", "Mystery" }
+};
+Console.WriteLine(book.Display());
+
+
 
 logger.Info("End!");
--- media.cs
+++ media.cs
@@ -26,3 +26,26 @@ public class Movie : Media
         return $"Id: {mediaId}\nTitle: {title}\nDirector: {director}\nRun time: {runningTime}\nGenres: {string.Join(", ", genres)}\n";
     }
 }
+
+public class Album : Media
+{
+    public string artist { get; set; }
+    public string recordLabel { get; set; }
+
+    public override string Display()
+    {
+        return $"Id: {mediaId}\nTitle: {title}\nArtist: {artist}\nLabel: {recordLabel}\nGenres: {string.Join(", ", genres)}\n";
+    }
+}
+
+public class Book : Media
+{
+    public string author { get; set; }
+    public UInt16 pageCount { get; set; }
+    public string publisher { get; set; }
+
+    public override string Display()
+    {
+        return $"Id: {mediaId}\nTitle: {title}\nAuthor: {author}\nPages: {pageCount}\nPublisher: {publisher}\nGenres: {string.Join(", ", genres)}\n";
+    }
+}
